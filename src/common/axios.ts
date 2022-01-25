bc692cd1cf8c3ab65dc3e1dbb0ed2a45f49ffce4
tadoriiii <tw772@naver.com>
2022-01-26 02:37:18 +0900

    add : axios search

---
+++ src/common/axios.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/search",
+});
+
+interface TRequest {
+  q: string;
+  part: string;
+  key: string;
+  maxResults: string;
+}
+
+export const onRequestSearch = async ({
+  q,
+  part,
+  key,
+  maxResults,
+}: TRequest) => {
+  try {
+    return await instance.get(
+      `/?q=${q}&part=${part}&key=${key}&maxResults=${maxResults}`
+    );
+  } catch (error: any) {
+    console.log(error);
+    throw error.data;
+  }
+};
--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { useWindowSize } from "react-use";
 
 import { Header } from "./Header";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 export const Landing = () => {
-  return <Header />;
+  const { height } = useWindowSize();
+
+  return (
+    <div className="LandingJSX" style={{ height: height }}>
+      <Header />
+      <div className="slickPosition"></div>
+    </div>
+  );
 };
